66e54d9cd25f1eda0f35dc2ac1d4d962621a43b0
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-05-26 14:46:33 +0000

    feat: added skeleton code for the API routes

--- web/backend/app.py
+++ web/backend/app.py
@@ -2,7 +2,7 @@
 import os
 from logging.config import dictConfig
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 dictConfig(
     {
@@ -26,6 +26,60 @@ dictConfig(
 app = Flask(__name__)
 app.config.from_prefixed_env()
 log = app.logger
+
+###### ROUTES #####
+@app.route("/", methods=("GET",))
+def list_all_clinics():
+    # TODO
+    log.debug("Listing all clinics...")
+    return jsonify({"message": "Clinics: ..."}), 501  # Change this to 200 (OK) when implemented
+
+
+@app.route("/c/<clinica>/registar", methods=("POST",))
+def schedule_appointment(clinica):
+    # TODO
+    data = request.get_json()
+    print(data)
+
+    pacient = data.get("pacient")
+    doctor = data.get("doctor")
+    timestamp = data.get("timestamp")
+
+    if pacient is None or doctor is None or timestamp is None:
+        return jsonify({"message": "Bad Request"}), 400
+
+    log.debug(f"Scheduling appointment at clinic \'{clinica}\'. Payload: {data}")
+    return data, 501  # Change this to 200 (OK) when implemented
+
+
+@app.route("/c/<clinica>/cancelar", methods=("POST",))
+def cancel_appointment(clinica):
+    # TODO
+    data = request.get_json()
+
+    pacient = data["pacient"]
+    doctor = data["doctor"]
+    timestamp = data["timestamp"]
+
+    if not pacient or not doctor or not timestamp:
+        return jsonify({"message": "Bad Request"}), 400
+
+    log.debug(f"Cancelling appointment at clinic \'{clinica}\'. Payload: {data}")
+    return data, 501  # Change this to 200 (OK) when implemented
+
+
+@app.route("/c/<clinica>", methods=("GET",))
+def list_clinic_specialties(clinica):
+    # TODO
+    log.debug(f"Listing specialties of clinic \'{clinica}\'")
+    return jsonify({"message": f"Specialties of clinic \'{clinica}\': ..."}), 501  # Change this to 200 (OK) when implemented
+
+
+@app.route("/c/<clinica>/<especialidade>", methods=("GET",))
+def list_specialty_doctors(clinica, especialidade):
+    # TODO
+    log.debug(f"Listing \'{especialidade}\' doctors from clinic \'{clinica}\'")
+    return jsonify({"message": f"\'{especialidade}\' doctors from clinic \'{clinica}\': ..."}), 501  # Change this to 200 (OK) when implemented
 
 
 @app.route("/ping", methods=("GET",))
