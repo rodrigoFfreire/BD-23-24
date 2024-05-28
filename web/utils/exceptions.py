3f3ad205d773264338d649d973b261dae040f59a
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-05-28 16:03:00 +0100

    fix: incorrect default value for NonExistentValue Exception

--- web/utils/exceptions.py
+++ web/utils/exceptions.py
@@ -10,7 +10,7 @@ class InvalidInput(AppExceptions):
 
 class NonExistentValue(AppExceptions):
     def __init__(self, msg="Non existent value") -> None:
-        if msg != "Invalid Input":
+        if msg != "Non existent value":
             self.msg = f"Non existent value: {msg}"
         self.msg = msg
         super().__init__(self.msg)
